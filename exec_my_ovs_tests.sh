c5c4b4bfae8d39a6e03fced25f00bb820f761917
Rick Alongi <ralongi@ralongi-thinkpadp16vgen1.rmtusma.csb>
2025-11-11 00:00:01 -0500

    Various changes

--- exec_my_ovs_tests.sh
+++ exec_my_ovs_tests.sh
@@ -266,7 +266,7 @@ export GRE_IPV6_TESTS="ovs_test_gre_ipv6 ovs_test_gre1_ipv6 ovs_test_gre_flow_ip
 #./test_exec_sanity_check.sh
 #./test_exec_vm100.sh
 #./test_exec_ovs_qos.sh
-#./test_exec_mcast_snoop.sh
+./test_exec_mcast_snoop.sh
 #./test_exec_power_cycle_crash.sh
 #./test_exec_forward_bpdu.sh
 #./test_exec_of_rules.sh
@@ -336,7 +336,7 @@ export GRE_IPV6_TESTS="ovs_test_gre_ipv6 ovs_test_gre1_ipv6 ovs_test_gre_flow_ip
 #./test_exec_topo.sh nfp ovs_env=kernel
 ##./test_exec_topo.sh nfp ovs_env=ovs-dpdk
 
-./test_exec_ovs_memory_leak_soak.sh
+#./test_exec_ovs_memory_leak_soak.sh
 #./test_exec_ovn_memory_leak_soak.sh
 
 #./test_exec_regression_bug.sh
