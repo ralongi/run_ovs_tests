3530fdf5786e64bcf2bcf2b1e144c93a2f530eb8
Rick Alongi <ralongi@ralongi-thinkpadp16vgen1.rmtusma.csb>
2026-01-19 00:00:01 -0500

    Various changes

--- exec_my_ovs_tests.sh
+++ exec_my_ovs_tests.sh
@@ -126,13 +126,13 @@ get_latest_driverctl()
 get_latest_driverctl
 
 if [[ -z $RPM_DRIVERCTL ]]; then
-	export RPM_DRIVERCTL=$DRIVERCTL_RHEL10
+	export RPM_DRIVERCTL=$DRIVERCTL_RHEL9
 fi
 if [[ -z $RPM_OVS_TCPDUMP_PYTHON ]]; then
-	export RPM_OVS_TCPDUMP_PYTHON=$OVS360_PYTHON_2511_RHEL10
+	export RPM_OVS_TCPDUMP_PYTHON=$OVS360_PYTHON_2511_RHEL9
 fi
 if [[ -z $RPM_OVS_TCPDUMP_TEST ]]; then
-	export RPM_OVS_TCPDUMP_TEST=$OVS360_TCPDUMP_2511_RHEL10
+	export RPM_OVS_TCPDUMP_TEST=$OVS360_TCPDUMP_2511_RHEL9
 fi
 
 # RHEL composes
@@ -226,33 +226,33 @@ export SRC_NETPERF="http://netqe-infra01.knqe.eng.rdu2.dc.redhat.com/share/tools
 
 # VM image names
 if [[ -z $VM_IMAGE ]]; then
-	export VM_IMAGE="rhel10.qcow2"
+	export VM_IMAGE="rhel9.qcow2"
 else
 	export VM_IMAGE=$VM_IMAGE
 fi
 
 if [[ -z $VM_IMAGE_AARCH64 ]]; then
-	export VM_IMAGE_AARCH64="rhel10.aarch64.64k.qcow2"
+	export VM_IMAGE_AARCH64="rhel9.aarch64.64k.qcow2"
 else
 	export VM_IMAGE_AARCH64=$VM_IMAGE_AARCH64
 fi
 
 # OVS packages
 if [[ -z $RPM_OVS ]]; then
-	export RPM_OVS=$OVS360_2511_RHEL10
+	export RPM_OVS=$OVS360_2511_RHEL9
 else
 	export RPM_OVS=$RPM_OVS
 fi
 
 # SELinux packages
 if [[ -z $RPM_OVS_SELINUX_EXTRA_POLICY ]]; then
-	export RPM_OVS_SELINUX_EXTRA_POLICY=$OVS_SELINUX_2511_RHEL10
+	export RPM_OVS_SELINUX_EXTRA_POLICY=$OVS_SELINUX_2511_RHEL9
 else
 	export RPM_OVS_SELINUX_EXTRA_POLICY=$RPM_OVS_SELINUX_EXTRA_POLICY
 fi
 
 # OVN packages
-export RPM_OVN=$OVN360_2511_RHEL10 
+export RPM_OVN=$OVN360_2511_RHEL9 
 
 export BONDING_TESTS="ovs_test_bond_active_backup ovs_test_bond_set_active_slave ovs_test_bond_lacp_active ovs_test_bond_lacp_passive ovs_test_bond_balance_slb ovs_test_bond_balance_tcp"
 
